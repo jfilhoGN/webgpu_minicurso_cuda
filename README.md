f99d585f56aec32b22348a2a5cf13b59e977da4f
João Martins Filho <joaomfilho1995@gmail.com>
2017-07-09 21:04:26 -0300

    Adicionando Login

---
+++ README.md
@@ -0,0 +1,9 @@
+Projeto WebGPU - SEINFO 2017
+----
+
+
+Desenvolvimento de uma plataforma online para implementação e execução de algoritmos na ferramenta CUDA para a III Seinfo 2017
+
+
+Aluno: João Martins Filho
+Professor: Rogério Aparecido Gonçalves
--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os, subprocess, sys, time
 from subprocess import check_output
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
 from werkzeug import secure_filename
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///webgpu.db', echo=True)
 
 app = Flask(__name__)
 
@@ -13,8 +17,40 @@ def allowed_file(filename):
     filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
 @app.route('/')
-def index():
+def first():
     return render_template('index.html')
+
+
+# PARTE DE LOGIN
+'''
+@app.route('/')
+def first():
+    if not session.get('logado'):
+            return render_template("login-form/login.html")
+    else:
+         return redirect(url_for('index'))
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    post_ra = int(request.form['ra'])
+    post_password = str(request.form['senha'])
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.ra==post_ra, User.password ==post_password)
+    result = query.first()
+    if result:
+        session['logado'] = True
+        #return render_template("index.html")
+    else:
+        flash("Senha ou RA errados!")
+    return login()
+
+@app.route('/logout')
+def logout():
+    session['logado'] = False
+    return login()
+'''
 
 @app.route('/exercicio1/')
 def exercicio1():
@@ -52,19 +88,21 @@ def uploaded_file(filename):
         out,err = q.communicate()
         return out
     elif(extension=="cu"):
-    	#nvcc -I /usr/local/cuda/include -L /usr/local/cuda/lib64 -ccbin=g++-4.9 src/sumvector.cu -lcuda -lm -o sumvector-cuda
-    	exe = ["/usr/local/cuda/bin/nvcc -I /usr/local/cuda/include -L /usr/local/cuda/lib64 -ccbin=g++-4.9 ./uploads/"+filename +" -lcuda -lm -o ./uploads/"+filename+".exe"]
-    	p = subprocess.Popen(exe, stdout = subprocess.PIPE, shell=True)
-    	p.communicate()
-    	exe1 = ["./uploads/"+filename+".exe"]
+        #nvcc -I /usr/local/cuda/include -L /usr/local/cuda/lib64 -ccbin=g++-4.9 src/sumvector.cu -lcuda -lm -o sumvector-cuda
+        exe = ["/usr/local/cuda/bin/nvcc -I /usr/local/cuda/include -L /usr/local/cuda/lib64 -ccbin=g++-4.9 ./uploads/"+filename +" -lcuda -lm -o ./uploads/"+filename+".exe"]
+        p = subprocess.Popen(exe, stdout = subprocess.PIPE, shell=True)
+        p.communicate()
+        exe1 = ["./uploads/"+filename+".exe"]
         q = subprocess.Popen(exe1, stdout = subprocess.PIPE, shell=True)
         out,err = q.communicate()
         #print out
         return out
 
 if __name__ == '__main__':
+    #ip host = 172.16.255.169
+    app.secret_key = os.urandom(12)
     app.run(
-        host="172.16.255.169",
+        host="localhost",
         port=int("5000"),
         debug=True
     )
