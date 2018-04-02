4cfd2e078808e0a3e029b32851b1aa8e7cba57f3
João Martins Filho <joaomfilho1995@gmail.com>
2018-04-02 17:06:47 -0300

    Criando telas de exercicios

--- versao_django/webgpu/views.py
+++ versao_django/webgpu/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, HttpResponse
 from django.template import loader
 
 def home(request):
-	#template = loader.get_template('home.html')
 	return render(request, 'home.html')
 
 def login(request):
