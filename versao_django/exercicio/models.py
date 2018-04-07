5aaadd7f187a059d96c1a864074e93a0cb43b589
João Martins Filho <joaomfilho1995@gmail.com>
2018-04-07 20:49:41 -0300

    adicionando modificações html exercicios

--- versao_django/exercicio/models.py
+++ versao_django/exercicio/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Exercicio(models.Model):
-    numeroExercicio = models.IntegerField(blank=False)
+    numeroExercicio = models.CharField(max_length=4, blank=False)
     descricao = models.CharField(max_length=100, blank=False)
+    codigo = models.CharField(max_length=1000, blank=False)
     resposta = models.CharField(max_length=100, blank=False)
     # Pesquisar como funciona ForeignKey
     #Usuario_idUsuario = models.ForeignKey(Usuario)
