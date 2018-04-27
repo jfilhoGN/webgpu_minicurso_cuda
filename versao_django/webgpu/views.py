be0574575523097405b441a92aeb632a22fc3a6f
João Martins Filho <joaomfilho1995@gmail.com>
2018-04-27 15:31:27 -0300

    Adicionando exercicios

--- versao_django/webgpu/views.py
+++ versao_django/webgpu/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
 from django.template import loader
+from django.views.decorators.http import require_POST
+
 
 def home(request):
 	return render(request, 'home.html')
 
 def login(request):
 	return render(request, 'login.html')
+
+
+# @require_POST
+# def entrar(request):
+#     usuario_aux = User.objects.get(email=request.POST['email'])
+#     usuario = authenticate(username=usuario_aux.username,
+#                            password=request.POST["senha"])
+#     if usuario is not None:
+#         login(request, usuario)
+#         return HttpResponseRedirect('/home/')
+
+#     return HttpResponseRedirect('/')
